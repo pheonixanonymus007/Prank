0c9e37832286920859b2798c8c0fd30fcb36e5bb
pheonixanonymus007 <96486609+pheonixanonymus007@users.noreply.github.com>
2023-02-08 15:54:47 +0530

    Add files via upload

--- Aman.py
+++ Aman.py
@@ -2,6 +2,10 @@ import webbrowser #webbrowser tool
 
 Name = input("Enter your name(first letter capital)  ") #taking name
 if Name == 'Aman': #checking if name
-    webbrowser.open_new_tab('https://images.freeimages.com/images/large-previews/4d0/hi-donkey-1402471.jpg')
+    webbrowser.open_new_tab('https://images.freeimages.com/images/large-previews/4d0/hi-donkey-1402471.jpg') #showing the image
+elif Name == 'Aman ':
+    webbrowser.open_new_tab('https://images.freeimages.com/images/large-previews/4d0/hi-donkey-1402471.jpg') #showing the image
 else:
-    print("Not for you")
+    webbrowser.open_new_tab('https://youtu.be/dQw4w9WgXcQ') # rr link
+    print("Not for you") 
+    
